b39c7cafaa787ec6c0aa3ca52dc250f80925322d
yishuai <yis@nes.com.cn>
2020-12-02 19:50:00 +0800

    创建集合项目

---
+++ CollectionProj/src/cn/itcast/learn/collection/demo/CollectionDemo.java
@@ -0,0 +1,40 @@
+package cn.itcast.learn.collection.demo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @Description TODO
+ * @Author yishuai
+ * @Date 2020/12/2 下午7:22
+ **/
+public class CollectionDemo {
+
+    public static void main(String[] args) {
+        Collection coll = new ArrayList();
+
+        show(coll);
+    }
+
+
+    public static void show(Collection coll){
+
+        //添加元素
+        coll.add("abcd1");
+        coll.add("abcd2");
+        coll.add("abcd3");
+        System.out.println(coll);
+
+        //删除
+        coll.remove("abcd2");
+        System.out.println(coll);
+
+        //清空集合
+//        coll.clear();
+
+        boolean contains = coll.contains("abcd2");
+        System.out.println(contains);
+    }
+
+
+}
---
+++ CollectionProj/src/cn/itcast/learn/collection/demo/IteratorDemo.java
@@ -0,0 +1,35 @@
+package cn.itcast.learn.collection.demo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * @Description TODO
+ * @Author yishuai
+ * @Date 2020/12/2 下午7:34
+ **/
+public class IteratorDemo {
+
+    public static void main(String[] args) {
+
+        Collection coll = new ArrayList();
+
+        coll.add("abc1");
+        coll.add("abc2");
+        coll.add("abc3");
+        coll.add("abc4");
+
+//        Iterator iterator = coll.iterator();
+//        while(iterator.hasNext()){
+//            System.out.println(iterator.next());
+//        }
+
+        for(Iterator it = coll.iterator(); it.hasNext();){
+            System.out.println(it.next());
+        }
+
+    }
+
+
+}
